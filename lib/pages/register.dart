59d43ef35227ef2fa0f58fab2c44f8a24feacb31
zalanirbhay21@gmail.com <zalanirbhay21@gmail.com>
2026-03-08 21:29:53 +0530

    creating the register page for user

---
+++ lib/pages/register.dart
@@ -0,0 +1,318 @@
+import 'package:flutter/material.dart';
+
+class register extends StatefulWidget {
+  const register({super.key});
+
+  @override
+  State<register> createState() => _registerState();
+}
+
+class _registerState extends State<register> {
+  bool isPasswordHidden = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Color(0xFFDFF5EA),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.black87),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: const Text("Be Healthy"),
+      ),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFFDFF5EA), Color(0xFFF5F5F5)],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: Center(
+            child: Padding(
+              padding: EdgeInsets.only(
+                top: MediaQuery.of(context).size.height * 0.08,
+                bottom: 30,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Create Account",
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black87,
+                    ),
+                  ),
+
+                  SizedBox(height: 30),
+                  Container(
+                    width: 320,
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black26,
+                          blurRadius: 10,
+                          offset: Offset(0, 5),
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Full Name",
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+
+                            SizedBox(height: 8),
+
+                            TextField(
+                              decoration: InputDecoration(
+                                hintText: "John Doe",
+                                prefixIcon: Icon(Icons.person),
+
+                                filled: true,
+                                fillColor: Color(0xFFF5F5F5),
+
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(30),
+                                  borderSide: BorderSide.none,
+                                ),
+                              ),
+                            ),
+                            Text(
+                              "Email address",
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+
+                            SizedBox(height: 8),
+
+                            TextField(
+                              decoration: InputDecoration(
+                                hintText: "hello@behealth.app",
+                                prefixIcon: Icon(Icons.email_outlined),
+
+                                filled: true,
+                                fillColor: Color(0xFFF5F5F5),
+
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(30),
+                                  borderSide: BorderSide.none,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 20),
+                            Text(
+                              "Password",
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+
+                            SizedBox(height: 8),
+
+                            TextField(
+                              obscureText: isPasswordHidden,
+                              decoration: InputDecoration(
+                                hintText: "••••••••",
+
+                                prefixIcon: Icon(Icons.lock_outline),
+
+                                suffixIcon: IconButton(
+                                  icon: Icon(
+                                    isPasswordHidden
+                                        ? Icons.visibility_off
+                                        : Icons.visibility,
+                                  ),
+                                  onPressed: () {
+                                    setState(() {
+                                      isPasswordHidden = !isPasswordHidden;
+                                    });
+                                  },
+                                ),
+
+                                filled: true,
+                                fillColor: Color(0xFFF5F5F5),
+
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(30),
+                                  borderSide: BorderSide.none,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 20),
+                            Text(
+                              "Confirm Password",
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+
+                            SizedBox(height: 8),
+
+                            TextField(
+                              obscureText: isPasswordHidden,
+                              decoration: InputDecoration(
+                                hintText: "••••••••",
+
+                                prefixIcon: Icon(Icons.lock_outline),
+
+                                suffixIcon: IconButton(
+                                  icon: Icon(
+                                    isPasswordHidden
+                                        ? Icons.visibility_off
+                                        : Icons.visibility,
+                                  ),
+                                  onPressed: () {
+                                    setState(() {
+                                      isPasswordHidden = !isPasswordHidden;
+                                    });
+                                  },
+                                ),
+
+                                filled: true,
+                                fillColor: Color(0xFFF5F5F5),
+
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(30),
+                                  borderSide: BorderSide.none,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 30),
+
+                            SizedBox(
+                              width: double.infinity,
+                              height: 50,
+
+                              child: ElevatedButton(
+                                onPressed: () {},
+
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.green,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(30),
+                                  ),
+                                ),
+
+                                child: Text(
+                                  "Sign Up",
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 20),
+                            Row(
+                              children: [
+                                Expanded(child: Divider()),
+
+                                Padding(
+                                  padding: EdgeInsets.symmetric(horizontal: 10),
+                                  child: Text(
+                                    "Or continue with",
+                                    style: TextStyle(
+                                      fontSize: 13,
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                ),
+
+                                Expanded(child: Divider()),
+                              ],
+                            ),
+                            SizedBox(height: 20),
+
+                            SizedBox(
+                              width: double.infinity,
+                              height: 50,
+
+                              child: OutlinedButton.icon(
+                                onPressed: () {},
+
+                                icon: SizedBox(
+                                  height: 24,
+                                  width: 24,
+                                  child: Image.asset(
+                                    "assets/images/logos.png",
+                                    fit: BoxFit.contain,
+                                  ),
+                                ),
+
+                                label: Text(
+                                  "Google",
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.black,
+                                  ),
+                                ),
+
+                                style: OutlinedButton.styleFrom(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(30),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 20),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Already have an account?",
+                        style: TextStyle(color: Colors.black54),
+                      ),
+
+                      TextButton(
+                        onPressed: () {},
+
+                        child: Text(
+                          "Login",
+                          style: TextStyle(
+                            color: Colors.green,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
