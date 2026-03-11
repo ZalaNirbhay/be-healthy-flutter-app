e3ce44eb2e6a581f499151dfa9e18f94e84ce940
zalanirbhay21@gmail.com <zalanirbhay21@gmail.com>
2026-03-11 09:19:53 +0530

    added gender , height weight and activity level selection

--- lib/pages/profile_setup.dart
+++ lib/pages/profile_setup.dart
@@ -8,6 +8,7 @@ class profile_setup extends StatefulWidget {
 }
 
 class _profile_setupState extends State<profile_setup> {
+  String activityLevel = "Moderate Exercise";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,6 +31,7 @@ class _profile_setupState extends State<profile_setup> {
         width: double.infinity,
         decoration: BoxDecoration(
           gradient: LinearGradient(
+            // colors: [Color(0xFFDFF5EA), Color(0xFFF5F5F5)],
             colors: [Color(0xFFDFF5EA), Color(0xFFF5F5F5)],
             begin: Alignment.topLeft,
             end: Alignment.bottomRight,
@@ -99,47 +101,202 @@ class _profile_setupState extends State<profile_setup> {
                       ),
                     ],
                   ),
-                  child: Column(children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Age",
-                              style: TextStyle(
-                                fontSize: 14,
-                                fontWeight: FontWeight.w500,
+                  child: Column(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+
+
+                          SizedBox(height: 8),
+
+                          TextField(
+                            decoration: InputDecoration(
+                              hintText: "Enter Your Age",
+                              prefixIcon: Icon(Icons.cake_rounded),
+
+                              filled: true,
+                              fillColor: Color(0xFFF5F5F5),
+
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: BorderSide.none,
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 30),
+
+                          // ignore: sized_box_for_whitespace
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Color(0xFFF5F5F5),
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                            width: double.infinity,
+                            child: Padding(
+                              padding: const EdgeInsets.only(
+                                top: 2,
+                                bottom: 2,
+                                left: 5,
+                                right: 5,
+                              ),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Expanded(
+                                    child: ElevatedButton(
+                                      onPressed: () {},
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor: Colors.white,
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(
+                                            30,
+                                          ),
+                                        ),
+                                      ),
+                                      child: Text("Male"),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: ElevatedButton(
+                                      onPressed: () {},
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor: Colors.white,
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(
+                                            30,
+                                          ),
+                                        ),
+                                      ),
+                                      child: Text("Female"),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 30),
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Color(0xFFF5F5F5),
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                            width: double.infinity,
+                            child: Padding(
+                              padding: const EdgeInsets.only(
+                                top: 2,
+                                bottom: 2,
+                                left: 5,
+                                right: 5,
+                              ),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Expanded(
+                                    child: TextField(
+                                      keyboardType: TextInputType.number,
+                                      decoration: InputDecoration(
+                                        prefixIcon: Icon(
+                                          Icons.height,
+                                          color: Colors.black,
+                                        ),
+                                        hintText: "Height",
+                                        filled: true,
+                                        fillColor: Colors.white,
+                                        contentPadding: EdgeInsets.symmetric(
+                                          vertical: 14,
+                                          horizontal: 15,
+                                        ),
+                                        border: OutlineInputBorder(
+                                          borderRadius: BorderRadius.circular(
+                                            30,
+                                          ),
+                                          borderSide: BorderSide.none,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(width: 8),
+                                  Expanded(
+                                    child: TextField(
+                                      keyboardType: TextInputType.number,
+                                      decoration: InputDecoration(
+                                        prefixIcon: Icon(
+                                          Icons.line_weight,
+                                          color: Colors.black,
+                                        ),
+
+                                        hintText: "Weight",
+                                        filled: true,
+                                        fillColor: Colors.white,
+                                        contentPadding: EdgeInsets.symmetric(
+                                          vertical: 14,
+                                          horizontal: 15,
+                                        ),
+                                        border: OutlineInputBorder(
+                                          borderRadius: BorderRadius.circular(
+                                            30,
+                                          ),
+                                          borderSide: BorderSide.none,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 30),
+                          Text(
+                            "Activity Level",
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+
+                          SizedBox(height: 8),
+
+                          DropdownButtonFormField<String>(
+                            value: activityLevel,
+
+                            decoration: InputDecoration(
+                              prefixIcon: Icon(Icons.directions_run),
+                              filled: true,
+                              fillColor: Color(0xFFF5F5F5),
+
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: BorderSide.none,
                               ),
                             ),
 
-                            SizedBox(height: 8),
+                            items:
+                                [
+                                  "Sedentary",
+                                  "Light Exercise",
+                                  "Moderate Exercise",
+                                  "Heavy Exercise",
+                                  "Athlete",
+                                ].map((level) {
+                                  return DropdownMenuItem(
+                                    value: level,
+                                    child: Text(level),
+                                  );
+                                }).toList(),
 
-                            TextField(
-                              decoration: InputDecoration(
-                                hintText: "Enter Your Age",
-                                prefixIcon: Icon(Icons.cake_rounded),
-
-                                filled: true,
-                                fillColor: Color(0xFFF5F5F5),
-
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(30),
-                                  borderSide: BorderSide.none,
-                                ),
-                              ),
-                            ),
-                           
-
-                            
-
-                          
-                               
-
-                           
-
-                          ],
-                        ),
-                      ],
-                    ),
+                            onChanged: (value) {
+                              setState(() {
+                                activityLevel = value!;
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
                 SizedBox(height: 20),
               ],
